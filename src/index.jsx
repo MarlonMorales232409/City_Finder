8e927a8d30349f682c5f9da5cd6fef0038184d4c
MarlonMorales232409 <marlon.morales2324@icloud.com>
2022-04-02 04:15:47 +0200

    APP DONE

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import CityFinder from "./CityFinder";
+import mapboxgl from "mapbox-gl";
+import "./globals.css";
+
+mapboxgl.accessToken = process.env.REACT_APP_ACCESS_TOKEN
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+ReactDOM.createRoot(rootElement).render(<CityFinder />);
